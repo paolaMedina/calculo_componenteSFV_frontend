333bf19f553ec53453f39a9c8dbf54a0a348b0a0
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-29 08:23:36 -0500

    Fv fields component moved to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SfvComponentsComponent } from './components/sfv-components/sfv-components.component';
 import { SharedModule } from './shared/shared.module';
-import { FvFieldsConfigurationComponent } from './fv-fields-configuration/fv-fields-configuration.component';
+import { FvFieldsConfigurationComponent } from '@app/components/fv-fields-configuration/fv-fields-configuration.component';
 import { CoreModule } from '@app/core/core.module';
 
 @NgModule({
