0a33361b51863a2e87128f26b3e815b5f309e33a
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-28 22:49:07 -0500

    Core module imported to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SfvComponentsComponent } from './components/sfv-components/sfv-components.component';
 import { SharedModule } from './shared/shared.module';
+import { FvFieldsConfigurationComponent } from './fv-fields-configuration/fv-fields-configuration.component';
+import { CoreModule } from '@app/core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SfvComponentsComponent
+    SfvComponentsComponent,
+    FvFieldsConfigurationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    CoreModule,
     SharedModule
   ],
   providers: [],
