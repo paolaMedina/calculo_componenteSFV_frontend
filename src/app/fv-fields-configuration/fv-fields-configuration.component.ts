df38029ee90c4d0b9eb9362244e251ebd8474df7
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-28 22:49:29 -0500

    Fv fields configuration component created

---
+++ src/app/fv-fields-configuration/fv-fields-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fv-fields-configuration',
+  templateUrl: './fv-fields-configuration.component.html',
+  styleUrls: ['./fv-fields-configuration.component.scss']
+})
+export class FvFieldsConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
