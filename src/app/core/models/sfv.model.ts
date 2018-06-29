e0883a9ac0f8e630ba484b47fb5137624666bfd5
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-28 22:50:33 -0500

    sfv model created

---
+++ src/app/core/models/sfv.model.ts
@@ -0,0 +1,11 @@
+export class Sfv {
+    power_of_plant_fv: number;
+    total_panels_fv: number;
+    power_of_panel_fv: number;
+    ambient_temperature: number;
+    lowest_ambient_temperature_expected: number;
+    investment_type: string;
+    service_type: string;
+    service_voltage: number;
+    instalation_place: string;
+}
