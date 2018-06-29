d4f21cf391c8ace2c232a53186f642d8d3c739ee
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-28 22:50:49 -0500

    Sfv form builder as a provider in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SfvFormBuilder } from '@app/core/forms';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [ SfvFormBuilder ]
 })
 export class CoreModule { }
