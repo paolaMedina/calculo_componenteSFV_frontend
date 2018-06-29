41ad352f2c567da942ce7dcb267fa97540b83a62
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-28 22:50:03 -0500

    Number of fileds fv form control added to sv form

--- src/app/core/forms/sfv.form.ts
+++ src/app/core/forms/sfv.form.ts
@@ -1,7 +1,9 @@
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomValidators } from './validators';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class SfvFormBuilder {
 
     constructor(private fb: FormBuilder) {
@@ -12,14 +14,13 @@ export class SfvFormBuilder {
             power_of_plant_fv: ['', Validators.required],
             total_panels_fv: ['', Validators.compose([Validators.required, CustomValidators.number])],
             power_of_panel_fv: ['', Validators.required],
+            number_of_fields_fv: ['', CustomValidators.number],
             ambient_temperature: ['', Validators.compose([Validators.required, CustomValidators.celsius])],
             lowest_ambient_temperature_expected: ['', Validators.compose([Validators.required, CustomValidators.celsius])],
             investment_type: ['', Validators.required],
             service_type: ['', Validators.required],
             service_voltage: ['', Validators.required],
-            instalation_place: ['', Validators.required],
-
-
+            instalation_place: ['', Validators.required]
         });
         return sfvForm;
     }
