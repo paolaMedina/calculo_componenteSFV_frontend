b901b53f8f8cba80215eb73cdc09fb95317e1dcc
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-30 15:09:52 -0500

    Cross validations and validations in each field

--- src/app/components/sfv-components/sfv-components.component.ts
+++ src/app/components/sfv-components/sfv-components.component.ts
@@ -4,6 +4,8 @@ import { FormGroup } from '@angular/forms';
 import { Sfv } from '@app/core/models';
 import { SfvFormBuilder } from '@app/core/forms';
 import { FvFieldService, SfvService } from '@app/core/services';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 export interface Food {
   value: string;
@@ -24,17 +26,32 @@ export class SfvComponentsComponent implements OnInit {
   ];
   constructor(
     private sfvFormBuilder: SfvFormBuilder,
-    private sfvService: SfvService
+    private sfvService: SfvService,
+    private router: Router,
+    public snackBar: MatSnackBar
   ) {
     this.sfv = this.sfvService.get();
     this.sfvForm = this.sfvFormBuilder.makeForm();
+
    }
+   openSnackBar(message: string, action: string) {
+    this.snackBar.open( message, action, {
+      duration: 2000,
+    });
+  }
    saveSfv() {
-     console.log(this.sfvForm.valid);
+     console.log(this.sfvFormBuilder.getAllErrors(this.sfvForm));
      this.sfvForm.markAsTouched();
+     this.sfvForm.get('investment_type').markAsTouched();
+     this.sfvForm.get('instalation_place').markAsTouched();
     if ( this.sfvForm.valid ) {
+      console.log(this.sfvForm.getError);
       this.sfv = this.sfvFormBuilder.extractData(this.sfvForm);
       this.sfvService.set(this.sfv);
+      this.router.navigate(['/fv-fields-config']);
+      console.log(this.sfvService.get());
+    } else {
+      this.openSnackBar("Se han enontrado algunos errores",  "Aceptar");
     }
    }
   ngOnInit() {
