42119b3a293cda40e9a03dc56ec75ac4a34e299f
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-06 16:13:12 -0500

    Routes to mppts configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { SfvComponentsComponent } from '@app/components/sfv-components/sfv-components.component';
 import { FvFieldsConfigurationComponent } from '@app/components/fv-fields-configuration/fv-fields-configuration.component';
 import { FvFieldConfigurationComponent } from '@app/components/fv-field-configuration/fv-field-configuration.component';
-
+import { MpptsConfigurationComponent } from './components/mppts-configuration/mppts-configuration.component';
+import { MpptCablingComponent } from '@app/components/mppt-cabling/mppt-cabling.component';
 const routes: Routes = [
   {
     path: '',
@@ -16,7 +17,15 @@ const routes: Routes = [
   {
     path: 'fv-field-config/:id',
     component: FvFieldConfigurationComponent
-  }
+  },
+  {
+    path: 'mppts-config/:fv_id',
+    component: MpptsConfigurationComponent
+  },
+  {
+    path: 'mppt-config/cabling/:fv_id',
+    component: MpptCablingComponent
+  },
 ];
 
 @NgModule({
