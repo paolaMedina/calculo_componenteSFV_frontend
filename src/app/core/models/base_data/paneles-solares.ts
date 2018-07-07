7e74ac498924536906922aad9086257e25645f60
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-07 17:32:19 -0500

    Added models from base_data

---
+++ src/app/core/models/base_data/paneles-solares.ts
@@ -0,0 +1,19 @@
+
+export class PanelSolar {
+     descripcion: string;
+     modelo: string;
+     fabricante: string;
+     tipo_celda: string;
+     no_de_celdas: number;
+     pmax: number;
+     eficiencia: string;
+     vmpp: any;
+     impp: string;
+     voc: any;
+     isc: string;
+     coef_voc: string;
+
+    
+}
+
+
