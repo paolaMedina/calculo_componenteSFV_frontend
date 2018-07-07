0586ec1b7ea3091cb628a4e732fdab78b40f43cd
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-07 17:48:39 -0500

    Get base data from backend

--- src/app/core/services/base-data.service.ts
+++ src/app/core/services/base-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ManualSwitch } from '@app/core/models';
-import { ApiService } from '@app/core/services/api.service';
+import { ManualSwitch, BaseData } from '../models';
+import { ApiService } from './api.service';
 import { Subject, Observable, of } from 'rxjs';
 import { debounceTime, tap } from 'rxjs/operators';
 
@@ -9,19 +9,36 @@ import { debounceTime, tap } from 'rxjs/operators';
 })
 export class BaseDataService {
   _manual_switches: ManualSwitch[];
-  constructor(private apiService: ApiService) { 
+  _baseData: BaseData;
+  constructor(private apiService: ApiService) {
 
   }
-  getManualSwithches(): Observable<ManualSwitch[]> {
-    if( this._manual_switches ) {
-      return of(this._manual_switches);
+  getBaseData(): Observable<BaseData> {
+    if (this._baseData) {
+      return of(this._baseData);
     } else {
-     return this.apiService.get('').pipe( tap(
-        (manual_switches: ManualSwitch[]) => {
-          this._manual_switches = manual_switches;
-        }
-      ))
+      return this.apiService.get('csvData')
+        .pipe(
+          tap(
+            (base_data: BaseData) => {
+              this._baseData = base_data;
+            }
+          )
+        );
     }
   }
-  
+  getManualSwithches(): Observable<ManualSwitch[]> {
+    if (this._manual_switches) {
+      return of(this._manual_switches);
+    } else {
+      return this.apiService.get('').
+        pipe(
+          tap(
+            (manual_switches: ManualSwitch[]) => {
+              this._manual_switches = manual_switches;
+            }
+          ))
+    }
+  }
+
 }
