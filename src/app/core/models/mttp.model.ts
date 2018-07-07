55b8a3a2e3e48f1490e0534e5c08e7eb100d50be
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-06 20:39:34 -0500

    Mttp model added

---
+++ src/app/core/models/mttp.model.ts
@@ -0,0 +1,17 @@
+export class Mttp {
+    /** Id, can be number `1...n` or if Mttp is combined id can be `1-2` or `3-4` or `n-n+1`  */
+    id: string;
+    /** Número de paneles en serie por cadena */
+    number_of_panels_in_series_per_chain: number;
+    /** Número de cadenas en paralelo */
+    number_of_chains_in_parallel;
+    /** Cableado */
+    cabling: any;
+    constructor(id: string) {
+        this.id = id;
+    }
+    static getCombinedName(id_first_mttp: string | number, id_second_mttp: string | number): string {
+        return `${id_first_mttp}-${id_second_mttp}`;
+    }
+
+}
