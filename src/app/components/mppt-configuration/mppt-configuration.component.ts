4b51f722f1adb5ba9a4a38a5a8f76b3613256a8b
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-06 20:36:59 -0500

    updated mppt configuration, now have mppt instance as input

--- src/app/components/mppt-configuration/mppt-configuration.component.ts
+++ src/app/components/mppt-configuration/mppt-configuration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Mttp } from '@app/core/models';
 
 @Component({
   selector: 'app-mppt-configuration',
@@ -6,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./mppt-configuration.component.scss']
 })
 export class MpptConfigurationComponent implements OnInit {
-  @Input() number: number;
+  @Input() mttp: Mttp;
   constructor() { }
 
   ngOnInit() {
--- src/app/components/mppts-configuration/mppts-configuration.component.ts
+++ src/app/components/mppts-configuration/mppts-configuration.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Mttp } from '@app/core/models';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-mppts-configuration',
@@ -6,13 +8,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mppts-configuration.component.scss']
 })
 export class MpptsConfigurationComponent implements OnInit {
-
-  constructor() { }
+  @Input() number_of_mttps = 5;
+  mttpsCombinationControl: FormControl;
+  mttps: Mttp[];
+  constructor() {
+    this.mttps = new Array<Mttp>();
+    this.mttpsCombinationControl = new FormControl();
+    this.mttpsCombinationControl.valueChanges.subscribe(value=> {
+      this.combineMttps(value);
+    });
+   }
   getArray(size: number): Array<Number> {
     
     return size? new Array<Number>(Number(size)): null;
   }
+
+  initMttps() {
+    for (let i = 0; i < this.number_of_mttps; i++) {
+      this.mttps.push(new Mttp(String(i+1)));
+    }
+  }
+  /**
+   *  
+   * @param combination String in format `n-n+1`, `mttps[n]` and `mttps[n+1]` should be combined in one mttp
+   */
+  combineMttps(combination: string){
+    console.log(combination, 'hola :v');
+    let id_mptt1: number;
+    let id_mptt2: number;
+    let newCombinedMptt: Mttp;
+    /** Extract ids of mptts from combination, example [5,6] */
+    let ids_mttps = combination.split('-');
+    /** Init ids of mptts from ids array */
+    id_mptt1 = Number(ids_mttps[0]);
+    id_mptt2 = Number(ids_mttps[2]);
+    /** Create a new mttp combined */
+    newCombinedMptt = new Mttp(Mttp.getCombinedName(id_mptt1, id_mptt2));
+    /** Delete the mttps to combine from `this.mttps` */
+    this.mttps = this.mttps.splice(id_mptt1,1);
+    console.log(this.mttps, 'mttps luego de borrar id_mptt1')
+    /** The first mttp for combination is deleted, the next mttp to be deleted now is at `id_mptt1` position, because the combinations only can be made if the mttp are contiguos */
+    this.mttps = this.mttps.splice(id_mptt1,2);
+    /** Add the new mttp combined at `id_mptt1` position */
+    this.mttps = this.mttps.splice(id_mptt1, 0, newCombinedMptt);
+  }
+  getArrayCombined(size: number): Array<string> {
+    let arrayCombined = new  Array<string>();
+    for(let i = 1; i <= size; i=i+2) {
+      const lower_bound = i ;
+      let separator: string;
+      let upper_bound: number | string;
+      if ( i >= size ) {
+        separator = ''
+        upper_bound = '';
+      } else {
+        separator = '-';
+        upper_bound = i + 1;
+      }
+      arrayCombined.push(`${lower_bound}${separator}${upper_bound}`);
+    }
+    console.log(arrayCombined);
+    return arrayCombined;
+  }
   ngOnInit() {
+    this.initMttps();
   }
 
 }
